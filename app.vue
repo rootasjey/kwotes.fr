3cb243486fb201bbac434abb2d76d2b00fcd0496
rootasjey <5097282+rootasjey@users.noreply.github.com>
2024-07-06 22:55:45 +0200

    fix: :lipstick: fix brightness mode: should always be light

--- app.vue
+++ app.vue
@@ -5,12 +5,9 @@
 </template>
 
 <script setup lang="ts">
-// import { useJune } from './hooks/useJune';
-import { AnalyticsBrowser } from '@june-so/analytics-next';
-const analytics = new AnalyticsBrowser();
-analytics.load({ writeKey: "JQVrRGiyoaFl4hQY" });
-analytics.track("page_view", { path: "/" });
-analytics.track('active users, retention, plan purchased, app downloaded, unique visitors')
-// console.log("Analytics", analytics);
-// const analytics = useJune("JQVrRGiyoaFl4hQY");
+  import { AnalyticsBrowser } from '@june-so/analytics-next';
+  const analytics = new AnalyticsBrowser();
+  analytics.load({ writeKey: "JQVrRGiyoaFl4hQY" });
+  analytics.track("page_view", { path: "/" });
+  analytics.track('active users, retention, plan purchased, app downloaded, unique visitors')
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,18 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'Kwotes App',
+      title: "Kwotes • App",
       meta: [
-        { name: 'apple-mobile-web-app-capable', content: 'yes' },
-        { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+        { name: "apple-mobile-web-app-capable", content: "yes" },
+        { name: "apple-mobile-web-app-status-bar-style", content: "black" },
       ]
     }
   },
-
-  css: ['~/assets/styles/main.css'],
+  colorMode: {
+    preference: "light"
+  },
+  css: ["~/assets/styles/main.css"],
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "nuxt-svgo", "@nuxt/image", "@nuxt/icon"],
-  compatibilityDate: '2024-07-06',
+  compatibilityDate: "2024-07-06",
 })
